6b6c5f5fbbcbbec043963a71e96a233127769eb4
Andrea Rodriguez <andrearodriguez@Andreas-MacBook-Pro.local>
2016-11-27 11:31:11 -0500

    tag favoritos

---
+++ app/src/main/java/com/example/andrearodriguez/adoptame/db/BebeDataBase.java
@@ -0,0 +1,8 @@
+package com.example.andrearodriguez.adoptame.db;
+
+/**
+ * Created by andrearodriguez on 11/22/16.
+ */
+
+public class BebeDataBase {
+}
---
+++ app/src/test/java/com/example/andrearodriguez/adoptame/entities/BebeTest.java
@@ -0,0 +1,10 @@
+package com.example.andrearodriguez.adoptame.entities;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by andrearodriguez on 11/17/16.
+ */
+public class BebeTest {
+
+}
